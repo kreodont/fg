6a1ba063b84c1d3f2587b74ad59f3ada6927761e
dima <dmitry@retailnext.net>
2017-11-20 15:40:39 +0300

    Fetch images script working

--- fetch_images.py
+++ fetch_images.py
@@ -1,4 +1,21 @@
 import requests
-url = 'https://www.dndbeyond.com/monsters'
-result = requests.get(url)
-print(result.text)
+import shutil
+from bs4 import BeautifulSoup
+
+for page_number in range(1, 42):
+    url = 'https://www.dndbeyond.com/monsters?page=%s' % page_number
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html5lib")
+    monsters_list = soup.find_all('div', {'data-type': 'monsters'})
+    for monster in monsters_list:
+        name = monster.find('a', {'class': 'link'}).contents[0]
+        image_div = monster.find('div', {'class': 'row monster-icon'}).find('a')
+        if not image_div:
+            print('%s has no picture' % name)
+            continue
+        image_link = image_div['href']
+        print('%s %s' % (name, image_link))
+        response = requests.get(image_link, stream=True)
+        with open('%s.jpg' % name, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
