92819664dfa9edf3a72bac91c9758a57840b12f0
dima <dmitry@retailnext.net>
2017-12-14 16:00:14 +0300

    Started working on pdf parsing

--- parse_pdf.py
+++ parse_pdf.py
@@ -5,16 +5,53 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfdevice import PDFDevice
+from pdfminer.layout import LAParams
+from pdfminer.converter import TextConverter
+from io import StringIO
 
 
-raw_file = open('bestiary.pdf', 'rb')
-parser = PDFParser(raw_file)
-document = PDFDocument(parser)
-if not document.is_extractable:
-    raise PDFTextExtractionNotAllowed
-rsrcmgr = PDFResourceManager()
-device = PDFDevice(rsrcmgr)
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-for page in PDFPage.create_pages(document):
-    print(interpreter.process_page(page))
-    exit(0)
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = {256}
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+
+text = convert_pdf_to_txt('king.pdf')
+with open('king.txt', 'w') as output_file:
+    output_file.write(text)
+
+# parser = PDFParser(raw_file)
+# document = PDFDocument(parser)
+# for i in range(1, 100):
+#     print(document.getobj(i))
+#
+# # for (level, title, dest, a, se) in document.g():
+# #     print(level, title, dest, a, se)
+# exit(0)
+# if not document.is_extractable:
+#     raise PDFTextExtractionNotAllowed
+# rsrcmgr = PDFResourceManager()
+# device = PDFDevice(rsrcmgr)
+# interpreter = PDFPageInterpreter(rsrcmgr, device)
+# for page in PDFPage.create_pages(document):
+#     print(page)
+#     print(interpreter.process_page(page))
+#     exit(0)
